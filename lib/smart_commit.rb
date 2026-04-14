dfdfedde177ddb1006583e54bf8764e713de9c06
Tom SCHIAVI <tom.schiavi@gmail.com>
2026-04-14 18:23:56 +0200

    :sparkles: feat: add main library entrypoint

---
+++ lib/smart_commit.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require_relative "smart_commit/version"
+require_relative "smart_commit/color"
+require_relative "smart_commit/hunk"
+require_relative "smart_commit/diff_parser"
+require_relative "smart_commit/git"
+require_relative "smart_commit/anthropic_api"
+require_relative "smart_commit/ui"
+require_relative "smart_commit/cli"
+
+module SmartCommit
+end
